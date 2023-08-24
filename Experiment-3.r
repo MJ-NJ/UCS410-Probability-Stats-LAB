7f6388b884c6022eb4df2fdb7a70e361896aad86
MJ-NJ <mjha_be21@thapar.edu>
2023-08-24 14:35:36 +0530

    Added assignment-3

---
+++ Experiment-3.r
@@ -0,0 +1,17 @@
+# Q.1
+# size <- 12
+# prob_success <- 1/6
+# lower_bound <- 7
+# upper_bound <- 9
+
+# probability <- pbinom(upper_bound, size, prob_success) - pbinom(lower_bound - 1, size, prob_success)
+
+# cat("Probability of getting 7, 8, or 9 sixes:", probability, "\n")
+
+# Q.2
+# mean <- 72
+# sd <- 15.2
+# x = pnorm(84,mean=72,sd=15.2,lower.tail=FALSE)
+# print(x)
+
+# Q.3
