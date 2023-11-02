8cc060348df721e6075cceb9ef0c93e32ac1cb6f
MJ-NJ <mjha_be21@thapar.edu>
2023-11-02 14:34:44 +0530

    Added q3

--- Experiment-7.r
+++ Experiment-7.r
@@ -14,6 +14,8 @@
 # }
 
 # Q.3
-x <- seq(6, -6, length(100))
+x <- seq(6, -6, length.out = 100)
 df_values <- c(1, 4, 10, 30)
 pdf_values <- sapply(df_values, function(df) dt(x, df))
+
+plot(x, pdf_values[, 4])
